6c67826e446843ea5ac87ef57cb236f0fb123a32
swapnil <swapnibrada@gmail.com>
2022-07-12 19:35:19 +0530

    dw

--- script_1.groovy
+++ script_1.groovy
@@ -1,3 +1,4 @@
+a = "rupam_nagpur"
 node {  
     stage('git clone') { 
         git 'https://github.com/swapnibrad/shubham_b.git'
@@ -5,7 +6,10 @@ node {
     stage('Build_vaibhav') { 
         sh 'pwd >> home.txt' 
     }
-    stage('Deploy') { 
+    stage('test') { 
         sh 'echo \'successfully deployed\' >> home.txt' 
+    }
+    stage('Deploy') { 
+        sh 'echo $a' 
     }
 }
