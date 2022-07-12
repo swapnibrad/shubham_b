b5e98a5ef31605bb60172cf9d6dc81464ba7abed
swapnibrad <91692042+swapnibrad@users.noreply.github.com>
2022-07-12 18:15:00 +0530

    dcs

---
+++ script_1.groovy
@@ -0,0 +1,12 @@
+a=swapnil
+node {  
+    stage('Build') { 
+        git 'https://github.com/swapnibrad/shubham_b.git' 
+    }
+    stage('Test') { 
+        echo 'successfully deployed build stage' 
+    }
+    stage('Deploy') { 
+        sh 'echo $a' 
+    }
+}
