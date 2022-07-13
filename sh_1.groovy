e07207add901681e5d7a8a190c8a8092abaffbd5
swapnil <swapnibrada@gmail.com>
2022-07-13 09:26:19 +0530

    cdds

--- sh_1.groovy
+++ sh_1.groovy
@@ -1,8 +1,21 @@
-job('test') {
-    steps {
-        shell('echo Hello World!')
+// job('test') {
+//     steps {
+//         shell('echo Hello World!')
+//     }
+//     steps {
+//         shell('echo good morning')
+//     }
+// }
+
+job('git item') {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/swapnibrad/shubham_b.git')
+            }
+        }
     }
     steps {
-        shell('echo good morning')
-    }
+         shell('echo successfully implemented item')
 }
